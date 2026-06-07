5c458dd4c5306e10ce4d53d03ce9753edf33c7a1
hadi arbabi <hadimojak@gmail.com>
2026-06-07 14:16:01 +0330

    array of obj

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,10 +2,78 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class ChirpController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
-        return view('home');
+        $chirps = [
+            (object) [
+                'author' => 'john doe',
+                'message' => 'just hellow',
+                'time' => '6 minutes ago',
+            ], (object) [
+                'author' => 'ali doe',
+                'message' => 'just hellow',
+                'time' => '2 minutes ago',
+            ], (object) [
+                'author' => 'hasan doe',
+                'message' => 'just hellow',
+                'time' => '3 minutes ago',
+            ]];
+
+        return view('home', ['chirps' => $chirps]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
